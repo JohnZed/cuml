f57d76a4374f1ac305517a10f66939c5c7f316db
John Zedlewski <jzedlewski@nvidia.com>
2020-01-07 16:37:28 -0800

    Expand hyper_dask script

--- python/cuml/experimental/hyperparams/hyper_dask.py
+++ python/cuml/experimental/hyperparams/hyper_dask.py
@@ -1,7 +1,9 @@
+"""PoC demo of dask-ml gridsearch with different estimators and options."""
 import cupy as cp
 import numpy as np
 import cudf
 import cuml
+import copy
 
 from dask.distributed import Client, wait
 import dask_ml.model_selection as dcv
@@ -10,19 +12,21 @@ from sklearn import datasets
 
 from sklearn.model_selection import train_test_split as sk_train_test_split, \
     GridSearchCV as sk_GridSearchCV
-
 from cuml.experimental.hyperparams.dummy import DummyModel
-from cuml.experimental.hyperparams.utils import *
-import cuml
 import sys
+import xgboost as xgb
+
+
+
 
 ESTIMATORS = {
-    'ridge': (cuml.Ridge(fit_intercept=True, solver="eig"), ["alpha"]),
-    'lasso': (cuml.Lasso(fit_intercept=False), ["alpha"]),
-    'elastic': (cuml.ElasticNet(fit_intercept=False), ["alpha"]),
-    'logistic': (cuml.LogisticRegression(fit_intercept=True, solver='qn'), ["C"]),
-    'qn': (cuml.QN(fit_intercept=True, loss="softmax"), ["l1_strength"]),
-    'rfr': (cuml.RandomForestRegressor(), ["min_impurity_decrease"]),
+    'ridge': (lambda: cuml.Ridge(fit_intercept=True, solver="eig"), ["alpha"]),
+    'lasso': (lambda: cuml.Lasso(fit_intercept=False), ["alpha"]),
+    'elastic': (lambda: cuml.ElasticNet(fit_intercept=False), ["alpha"]),
+    'logistic': (lambda: cuml.LogisticRegression(fit_intercept=True, solver='qn'), ["C"]),
+    'qn': (lambda: cuml.QN(fit_intercept=True, loss="softmax"), ["l1_strength"]),
+    'rfr': (lambda: cuml.RandomForestRegressor(), ["min_impurity_decrease"]),
+    'xgb': (lambda: xgb.XGBRegressor(n_estimators=10, tree_method="gpu_hist"), ["gamma"]),
     'dummy': (DummyModel(order='F'), ["alpha"])
 }
 
@@ -31,6 +35,17 @@ ESTIMATORS = {
 #
 N_FOLDS = 5
 SCALE_FACTOR = int(1e2)
+DTYPE = np.float32
+
+
+import time
+from contextlib import contextmanager
+@contextmanager
+def timed(txt):
+    t0 = time.time()
+    yield
+    t1 = time.time()
+    print("%32s time:  %8.5f" % (txt, t1 - t0))
 
 def set_rmm():
     import cudf
@@ -54,10 +69,10 @@ if __name__ == '__main__':
                                                            diabetes.target,
                                                            test_size=0.2)
 
-    dup_data = np.array(np.vstack([X_train]*SCALE_FACTOR))
-    dup_train = np.array(np.hstack([y_train]*SCALE_FACTOR))
-    dup_test_data = np.array(np.vstack([X_test]*SCALE_FACTOR))
-    dup_test_y = np.array(np.hstack([y_test]*SCALE_FACTOR))
+    dup_data = np.array(np.vstack([X_train]*SCALE_FACTOR), dtype=DTYPE)
+    dup_train = np.array(np.hstack([y_train]*SCALE_FACTOR), dtype=DTYPE)
+    dup_test_data = np.array(np.vstack([X_test]*SCALE_FACTOR), dtype=DTYPE)
+    dup_test_y = np.array(np.hstack([y_test]*SCALE_FACTOR), dtype=DTYPE)
 
     print(f"""Duplicated data in memory: {dup_data.nbytes / 1e6} MB
               (Scale factor {SCALE_FACTOR})""")
@@ -72,8 +87,24 @@ if __name__ == '__main__':
     gdf_test_data = cudf.DataFrame(record_test_data)
     gdf_test_y = cudf.DataFrame(dict(test=dup_test_y))
 
-    cu_clf, param_names = ESTIMATORS[estimator_name]
+    cu_clf_func, param_names = ESTIMATORS[estimator_name]
+    cu_clf = cu_clf_func()
     params = {param_names[0]: np.logspace(-3, -1, 10)}
+
+    # Start by computing SOL with no work done beyond model creation+fit+score
+    nreps_total = len(list(params.values())[0]) * N_FOLDS
+    sub_input_nrows = int(gdf_data.shape[0] * (1.0 - (1/float(N_FOLDS))))
+    sub_train_X = gdf_data.iloc[:sub_input_nrows, :]
+    sub_train_y = gdf_train.train[:sub_input_nrows]
+    sub_test_X = gdf_data.iloc[sub_input_nrows:, :]
+    sub_test_y = gdf_train.train[sub_input_nrows:]
+    nreps_total = len(list(params.values())[0]) * N_FOLDS
+    print(f"Running {nreps_total} reps to cover {N_FOLDS} folds")
+    with timed("nodask-SOL"):
+        for i in range(nreps_total):
+            cu_clf_rep = cu_clf_func()
+            cu_clf_rep.fit(sub_train_X, sub_train_y)
+            cu_clf_rep.score(sub_test_X, sub_test_y)
 
     with timed("Dask-nocache-CV5"):
         cu_grid = dcv.GridSearchCV(cu_clf, params, cv=N_FOLDS, cache_cv=False)
@@ -87,6 +118,3 @@ if __name__ == '__main__':
         res = cu_grid.fit(gdf_data, gdf_train.train)
         print("Best estimator was: ", cu_grid.best_estimator_)
 
-
-# Tested (pass): ridge
-# Tested (fail): lasso, elasticnet
